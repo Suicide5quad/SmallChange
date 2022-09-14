520806279dc4bf75b210e1e07681c1342b4288e6
Chaitanya <onlyawesome121@gmail.com>
2022-09-14 15:56:15 +0530

    history

--- SmallChange/src/app/tradinghistory/tradinghistory.component.ts
+++ SmallChange/src/app/tradinghistory/tradinghistory.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BuyTrade } from '../models/buyTrade';
+import { SellTrade } from '../models/sellTrade';
+import { HistoryService } from './history.service';
 
 @Component({
   selector: 'app-tradinghistory',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TradinghistoryComponent implements OnInit {
 
-  constructor() { }
+  public buytrade: BuyTrade[]=[]
+  constructor(private historyservice:HistoryService) { }
+  getBuyTrade()
+  {
+    this.historyservice.getBuyTrade().subscribe(data=>this.buytrade=data)
+   console.log(this.buytrade);
+  }
+  selltrade:SellTrade[]=[]
+  getSellTrade()
+  {
+    this.historyservice.getSellTrade().subscribe(data=>this.selltrade=data)
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
