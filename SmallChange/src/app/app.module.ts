90c7e4637683cc9f55dc9197dfbaf00febbf4fb6
Chaitanya <onlyawesome121@gmail.com>
2022-09-14 14:25:01 +0530

    history

--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TradinghistoryComponent } from './tradinghistory/tradinghistory.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent,
     HeaderComponent,
     FooterComponent,
+    TradinghistoryComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ SmallChange/src/app/tradinghistory/tradinghistory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tradinghistory',
+  templateUrl: './tradinghistory.component.html',
+  styleUrls: ['./tradinghistory.component.css']
+})
+export class TradinghistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
