dd9ddb81b12557d5cf525fddc76a3bf8c1620c2d
Karthick <okayawesome121@gmail.com>
2022-09-14 14:39:55 +0530

    Home page changes

--- SmallChange/src/app/app-routing.module.ts
+++ SmallChange/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomePageComponent },
+  { path: 'Login', component: LoginPageComponent },
+  { path: '', redirectTo: '/Home', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+// export const routingComponents = [HomePageComponent, LoginPageComponent];
--- SmallChange/src/app/app.module.ts
+++ SmallChange/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent,
     HeaderComponent,
     FooterComponent,
+    HomePageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
