a8c63026d3cf2dbe69306d89eb8781ee26a14661
Karthick <okayawesome121@gmail.com>
2022-09-17 14:37:32 +0530

    added preferences to navbar

--- SmallChange/src/app/app-routing.module.ts
+++ SmallChange/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { AllTradesComponent } from './all-trades/all-trades.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { PortfolioPageComponent } from './portfolio/portfolio-page/portfolio-page.component';
-import { BuyTradeComponent } from './trades/buy-trade/buy-trade.component';
+import { SetInvestmentPreferencesComponent } from './set-investment-preferences/set-investment-preferences.component';
 
 const routes: Routes = [
   { path: 'Home', component: HomePageComponent },
   { path: 'Login', component: LoginPageComponent },
   { path: 'Portfolio', component: PortfolioPageComponent},
   { path: 'Trade', component: AllTradesComponent},
+  { path: 'Preferences', component: SetInvestmentPreferencesComponent},
   { path: '', redirectTo: '/Home', pathMatch: 'full' }
 ];
 
