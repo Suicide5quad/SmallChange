32e966aadad7f28f9b9362845f5131dad15d7ec7
Suicide5quad <okayawesome121@gmail.com>
2022-09-29 13:58:32 +0530

    test changes

--- SmallChange/src/app/tradinghistory/tradinghistory.component.spec.ts
+++ SmallChange/src/app/tradinghistory/tradinghistory.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  HttpTestingController,
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
 import { TradinghistoryComponent } from './tradinghistory.component';
 
 describe('TradinghistoryComponent', () => {
@@ -8,9 +11,9 @@ describe('TradinghistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TradinghistoryComponent ]
-    })
-    .compileComponents();
+      declarations: [TradinghistoryComponent],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
