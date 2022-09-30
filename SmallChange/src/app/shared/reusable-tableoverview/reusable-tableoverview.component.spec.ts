9a2ffbd3becfc96bec927d4d588fadfdba27f9fa
Ravi <okayawesome121@gmail.com>
2022-09-30 18:18:42 +0530

    testing changes

--- SmallChange/src/app/shared/reusable-tableoverview/reusable-tableoverview.component.spec.ts
+++ SmallChange/src/app/shared/reusable-tableoverview/reusable-tableoverview.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ReusableTableoverviewComponent } from './reusable-tableoverview.component';
+import { ReusableTableOverviewComponent} from './reusable-tableoverview.component';
 
 describe('ReusableTableoverviewComponent', () => {
-  let component: ReusableTableoverviewComponent;
-  let fixture: ComponentFixture<ReusableTableoverviewComponent>;
+  let component: ReusableTableOverviewComponent;
+  let fixture: ComponentFixture<ReusableTableOverviewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReusableTableoverviewComponent ]
+      declarations: [ ReusableTableOverviewComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReusableTableoverviewComponent);
+    fixture = TestBed.createComponent(ReusableTableOverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
