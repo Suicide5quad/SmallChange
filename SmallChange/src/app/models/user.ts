639e56a88ab6020f51221b61872082045139eb4e
Karthick <okayawesome121@gmail.com>
2022-10-02 23:50:35 +0530

    User Service & sign up page backend connection

---
+++ SmallChange/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  userId!: number;
+  firstName!: string;
+  lastName!: string;
+  emailId!: string;
+  dob!: Date;
+  phNo!: string;
+  balance!: number;
+  password!: string;
+}
